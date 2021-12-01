f3dc2ad5600a221be79abfef6c97321511591d05
Creepsy <Robin.gd@outlook.de>
2021-12-01 17:36:48 +0100

    Solved day 1

--- day1/program.hs
+++ day1/program.hs
@@ -1,1 +1,23 @@
-main = putStrLn "Initial commit"
+main = do
+    input <- readFile "input.txt"
+
+    let measurements = map read . lines $ input :: [Int]
+
+    putStrLn $ "Part 1: " ++ show (countHeightIncrements measurements)
+    putStrLn $ "Part 2: " ++ show (countThreeMeasurementIncrements measurements)
+
+countHeightIncrements :: [Int] -> Int
+countHeightIncrements [] = 0
+countHeightIncrements (first:rest)
+    | null rest = 0 
+    | otherwise  = countHeightIncrements rest + fromEnum (first < head rest)
+
+countThreeMeasurementIncrements :: [Int] -> Int 
+countThreeMeasurementIncrements measurements
+    | length measurements < 4 = 0
+    | otherwise = 
+        let 
+            firstGroup = take 3 measurements
+            secondGroup = take 3 . drop 1 $ measurements
+        in 
+            countThreeMeasurementIncrements (drop 1 measurements) + fromEnum (sum firstGroup < sum secondGroup)
